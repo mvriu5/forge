2ebd03c5b0854e9e3d4b333b584cc21c115d0d0d
Marius Ahsmus <marius.ahsmus@gmail.com>
2025-03-20 20:16:51 +0100

    init

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,17 @@
 import { betterAuth } from "better-auth"
+import {drizzleAdapter} from "better-auth/adapters/drizzle"
+import { db } from "@/database"
+import { schema } from "@/db/schema"
 
 export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema: {
+            ...schema,
+            user: schema.users,
+        }
+    }),
     emailAndPassword: {
         enabled: true
-    }
+    },
 })
