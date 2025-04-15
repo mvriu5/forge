aba7d724fa9aa36c532362bc4302a024fef52a27
Marius Ahsmus <marius.ahsmus@gmail.com>
2025-04-15 18:46:33 +0200

    feat: save & delete logic, more icons

--- src/components/svg/BookmarkIcons.tsx
+++ src/components/svg/BookmarkIcons.tsx
@@ -1,3 +1,4 @@
+import {File} from "lucide-react"
 
 const GoogleSheets = () => {
     return (
@@ -15,6 +16,87 @@ const GoogleSheets = () => {
     )
 }
 
+const GoogleSlides = () => {
+    return (
+        <svg
+            role="img"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill={"#FBBC04"}
+        >
+            <title>Google Slides</title>
+            <path d="M16.09 15.273H7.91v-4.637h8.18v4.637zm1.728-8.523h2.91v15.614c0 .904-.733 1.636-1.637 1.636H4.909a1.636 1.636 0 0 1-1.636-1.636V1.636C3.273.732 4.005 0 4.909 0h9.068v6.75h3.841zm-.363 2.523H6.545v7.363h10.91V9.273zm-2.728-5.979V6h6.001l-6-6v3.294z"/>
+        </svg>
+    )
+}
+
+const GoogleDocs = () => {
+    return (
+        <svg
+            role="img"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill={"#4285F4"}
+        >
+            <title>Google Docs</title>
+            <path d="M14.727 6.727H14V0H4.91c-.905 0-1.637.732-1.637 1.636v20.728c0 .904.732 1.636 1.636 1.636h14.182c.904 0 1.636-.732 1.636-1.636V6.727h-6zm-.545 10.455H7.09v-1.364h7.09v1.364zm2.727-3.273H7.091v-1.364h9.818v1.364zm0-3.273H7.091V9.273h9.818v1.363zM14.727 6h6l-6-6v6z"/>
+        </svg>
+    )
+}
+
+const Loom = () => {
+    return (
+        <svg
+            role="img"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill={"#625DF5"}
+        >
+            <title>Loom</title>
+            <path d="M24 10.665h-7.018l6.078-3.509-1.335-2.312-6.078 3.509 3.508-6.077L16.843.94l-3.508 6.077V0h-2.67v7.018L7.156.94 4.844 2.275l3.509 6.077-6.078-3.508L.94 7.156l6.078 3.509H0v2.67h7.017L.94 16.844l1.335 2.313 6.077-3.508-3.509 6.077 2.312 1.335 3.509-6.078V24h2.67v-7.017l3.508 6.077 2.312-1.335-3.509-6.078 6.078 3.509 1.335-2.313-6.077-3.508h7.017v-2.67H24zm-12 4.966a3.645 3.645 0 1 1 0-7.29 3.645 3.645 0 0 1 0 7.29z"/>
+        </svg>
+    )
+}
+
+const Youtube = () => {
+    return (
+        <svg
+            role="img"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill={"#FF0000"}
+            fillOpacity={0.8}
+        >
+            <title>YouTube</title>
+            <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+        </svg>
+    )
+}
+
+const getLogoFromLink = (link: string) => {
+    switch (true) {
+        case link.includes("docs.google.com/spreadsheets"): return <GoogleSheets />
+        case link.includes("docs.google.com/documents"): return <GoogleDocs />
+        case link.includes("docs.google.com/presentation"): return <GoogleSlides />
+        case link.includes("loom.com"): return <Loom />
+        case link.includes("youtube.com"): return <Youtube/>
+        default: return <File size={16}/>
+    }
+};
+
 export {
-    GoogleSheets
+    getLogoFromLink,
+    GoogleSheets,
+    GoogleSlides,
+    GoogleDocs,
+    Loom,
+    Youtube,
 }
--- src/components/widgets/BookmarkWidget.tsx
+++ src/components/widgets/BookmarkWidget.tsx
@@ -1,47 +1,149 @@
-import type React from "react"
+import React, {useState} from "react"
 import {WidgetProps, WidgetTemplate} from "@/components/widgets/WidgetTemplate"
 import {ScrollArea} from "@/components/ui/ScrollArea"
-import {GoogleSheets} from "@/components/svg/BookmarkIcons"
+import {getLogoFromLink} from "@/components/svg/BookmarkIcons"
 import {Button} from "@/components//ui/Button"
 import {Bookmark, Link, Trash} from "lucide-react"
 import {tooltip} from "@/components/ui/TooltipProvider"
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/Popover"
+import {Form, FormField, FormInput, FormItem, FormLabel, FormMessage} from "@/components/ui/Form"
+import {z} from "zod"
+import {useForm} from "react-hook-form"
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useWidgetStore} from "@/store/widgetStore"
+
+interface BookmarkItem {
+    title: string
+    link: string
+}
 
 const BookmarkWidget: React.FC<WidgetProps> = ({editMode, onWidgetDelete}) => {
+    const {refreshWidget} = useWidgetStore()
+    const widget = useWidgetStore(state => state.getWidget("bookmark"))
+    if (!widget) return null
+
+    const [bookmarks, setBookmarks] = useState<BookmarkItem[]>(widget.config?.bookmarks ?? [])
+    const [open, setOpen] = useState<boolean>(false)
+
+    const formSchema = z.object({
+        title: z.string(),
+        link: z.string().url()
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+            link: ""
+        },
+    })
+
+    const handleAdd = async (bookmark: BookmarkItem) => {
+        const updatedBookmarks = bookmarks.find(b => b.title === bookmark.title)
+            ? bookmarks
+            : [...bookmarks, bookmark];
+
+        setBookmarks(updatedBookmarks);
+
+        await refreshWidget({
+            ...widget,
+            config: {
+                bookmarks: updatedBookmarks
+            }
+        })
+
+        setOpen(false)
+        form.reset()
+    }
+
+    const handleDelete = async (title: string) => {
+        const updatedBookmarks = bookmarks.filter((b) => b.title !== title);
+
+        setBookmarks(updatedBookmarks);
+
+        await refreshWidget({
+            ...widget,
+            config: {
+                bookmarks: updatedBookmarks
+            }
+        })
+    }
 
     return (
         <WidgetTemplate className={"col-span-1 row-span-2"} name={"bookmark"} editMode={editMode} onWidgetDelete={onWidgetDelete}>
             <div className={"h-full flex flex-col gap-2"}>
                 <div className={"flex items-center justify-between gap-2"}>
                     <p className={"text-primary text-lg font-semibold"}>Bookmarks</p>
-                    <Button className={"bg-secondary gap-1"}>
-                        <Bookmark size={18}/>
-                        Create
-                    </Button>
+                    <Popover open={open} onOpenChange={setOpen}>
+                        <PopoverTrigger asChild>
+                            <Button className={"bg-secondary gap-1"}>
+                                <Bookmark size={18}/>
+                                Add
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent className={"w-56"} align={"end"}>
+                            <Form {...form}>
+                                <form onSubmit={form.handleSubmit(handleAdd)} className="space-y-4">
+                                    <FormField
+                                        control={form.control}
+                                        name="title"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Title</FormLabel>
+                                                <FormInput placeholder="Title" {...field} />
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="link"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>URL</FormLabel>
+                                                <FormInput placeholder="URL" {...field} />
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <Button
+                                        type="submit"
+                                        className={"bg-brand hover:bg-brand/90 text-primary w-full"}
+                                    >
+                                        Add link
+                                    </Button>
+
+                                </form>
+                            </Form>
+                        </PopoverContent>
+                    </Popover>
                 </div>
+                <div className={"w-full h-px border-b border-main/40"}></div>
                 <ScrollArea className={"h-full"} thumbClassname={"bg-white/5"}>
-                    <BookmarkItem title={"Tracking Sheets"} link={"https://docs.google.com/spreadsheets/d/1aaNnGiMpeqNONmiMwASHuMzlmdOzqEKTvSvQPv1fKIA"}/>
+                    {bookmarks.map((bookmark) => (
+                        <BookmarkItem key={bookmark.title} title={bookmark.title} link={bookmark.link} onDelete={() => handleDelete(bookmark.title)}/>
+                    ))}
                 </ScrollArea>
             </div>
         </WidgetTemplate>
     )
 }
 
-interface BookmarkProps {
-    title: string
-    link: string
-}
 
-const BookmarkItem = ({title, link}: BookmarkProps) => {
+const BookmarkItem = ({title, link, onDelete}: BookmarkItem & {onDelete: (title: string) => void}) => {
 
     const linkTooltip = tooltip<HTMLDivElement>({
         message: link
     })
 
     return (
-        <div className={"group w-full flex items-center gap-2 justify-between rounded-md hover:bg-secondary p-2 cursor-pointer overflow-hidden"}>
+        <div
+            className={"group w-full flex items-center gap-2 justify-between rounded-md hover:bg-secondary p-2 cursor-pointer overflow-hidden"}
+            onClick={() => window.open(link, '_blank', 'noopener,noreferrer')}
+        >
             <div className={"flex items-center gap-2"}>
                 <div className={"flex justify-center items-center p-1 rounded-md bg-white/10"}>
-                    <GoogleSheets/>
+                    {getLogoFromLink(link)}
                 </div>
                 <p className={"text-primary truncate"}>{title}</p>
                 <div className={"h-4 w-px border-r border-main"}/>
@@ -51,11 +153,16 @@ const BookmarkItem = ({title, link}: BookmarkProps) => {
 
             </div>
 
-            <Button className={"hidden group-hover:flex px-1 h-6"}>
+            <Button
+                className={"hidden group-hover:flex px-1 h-6"}
+                onClick={(e) => {
+                    e.stopPropagation()
+                    onDelete(title)
+                }}
+            >
                 <Trash size={14}/>
             </Button>
         </div>
-
     )
 }
 
