53b3b90ed1c9093006aa99353fd5c5bfb10a0e8c
Marius Ahsmus <marius.ahsmus@gmail.com>
2025-07-30 10:24:15 +0200

    fix: updated http only cookies in auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -78,7 +78,6 @@ export const auth = betterAuth({
     advanced: {
         useSecureCookies: true,
         defaultCookieAttributes: {
-            httpOnly: true,
             secure: true
         }
     },
