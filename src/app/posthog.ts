e0d5d93fa9df1419869f330a59f9c00cba683fd7
Marius Ahsmus <marius.ahsmus@gmail.com>
2025-12-20 16:08:03 +0100

    fix: removed posthog.debug

--- src/app/posthog.ts
+++ src/app/posthog.ts
@@ -2,10 +2,9 @@ import { PostHog } from 'posthog-node'
 
 export default function PostHogClient() {
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
     flushAt: 1,
     flushInterval: 0
   })
-  posthogClient.debug(true)
   return posthogClient
 }
