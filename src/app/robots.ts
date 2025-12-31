39efabe65d8dd08433fdd10b7f9733d464881775
Marius Ahsmus <marius.ahsmus@gmail.com>
2025-12-31 16:26:01 +0100

    fix: slash in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,7 @@ export default function Robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: "*",
-            allow: ["/", "/privacy", "/terms", "/imprint"],
+            allow: ["/", "/privacy/", "/terms/", "/imprint/"],
             disallow: ["/dashboard/", "/reference/", "/api/"]
         },
         host:  "www.tryforge.io",
